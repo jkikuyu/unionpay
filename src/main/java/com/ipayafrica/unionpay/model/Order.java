9cc59e6749aac4658b4c18b777bcbb6f076ab433
Jude Kikuyu <jkikuyu@gmail.com>
2019-10-18 01:05:31 +0300

    include transient and inheritance strategy to Order bean

--- src/main/java/com/ipayafrica/unionpay/model/Order.java
+++ src/main/java/com/ipayafrica/unionpay/model/Order.java
@@ -1,12 +1,49 @@
 package com.ipayafrica.unionpay.model;
 
+import java.util.Calendar;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "Order")
+@Inheritance(strategy=InheritanceType.JOINED)
 public class Order {
-	String orderId;
-	String txnTime;
-	String txnAmount;
+    @Id
+    @Basic(optional = false)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "ORDERID")
+
+	private String orderId;
+    @Column(name = "TRANSACTTIME", nullable=false)
+    @Temporal(TemporalType.TIMESTAMP)
+
+    private Calendar transactTime;
+    
+    @Column(name = "AMOUNT", nullable=false, precision=10, scale=2)
+    
+    private Double amount;
+    
+    @Transient
+    private String txnTime;
+    
+    @Transient
+	private String txnAmount;
+	
 	public String getOrderId() {
 		return orderId;
 	}
+
 	public String getTxnTime() {
 		return txnTime;
 	}
@@ -21,6 +58,23 @@ public class Order {
 	}
 	public void setTxnAmount(String txnAmount) {
 		this.txnAmount = txnAmount;
+	}
+
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public Calendar getTransactTime() {
+		return transactTime;
+	}
+
+	public void setTransactTime(Calendar transactTime) {
+		this.transactTime = transactTime;
 	}
 	
 	
